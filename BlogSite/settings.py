6c55a2532d7c3c07ac77df22c04f8ce79a05ae49
Dark Lord <bhosalepranil16@gmail.com>
2022-01-28 21:03:02 +0530

    init

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8jam5(ot+ir=who-r&i8+cyx$yir6w*f5po+_&8+&pbjk1of+n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
